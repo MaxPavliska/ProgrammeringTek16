d4ea8862ef3f43769cd34fb6a41110d91a01a8e2
MaxPavliska <maximillianlouis.pavliska@sundbyberg.it-gymnasiet.se>
2017-09-11 10:44:57 +0200

    Lesson classes

---
+++ src/lektion/klasser/Application.java
@@ -0,0 +1,13 @@
+package lektion.klasser;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		Person max = new Person();
+		
+	}
+
+}
